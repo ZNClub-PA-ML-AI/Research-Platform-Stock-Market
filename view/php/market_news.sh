06c0a520949677164d61123628a1bb686acaf1c5
ZNevzz <dsouza.nevil45@gmail.com>
2017-04-22 15:43:46 +0530

    shell script added

--- view/php/market_news.sh
+++ view/php/market_news.sh
@@ -1,5 +1,14 @@
 
 #run python scripts for market-news module
 
+#take company_id as input
+company_id=$1
+
+python ../../module/market-news/src/model/linear_model.py $company_id
+
+#end of file
+
+
+
 
 
