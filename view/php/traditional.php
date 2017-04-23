516b5bcae1d0407163d574534ae261a5f3d7fa4e
ZNevzz <dsouza.nevil45@gmail.com>
2017-04-23 01:29:34 +0000

    on behalf of @melwyn95

--- view/php/traditional.php
+++ view/php/traditional.php
@@ -4,5 +4,6 @@ header('Access-Control-Allow-Origin: *');
 $company = $_GET['company'];
 //echo './traditional.sh '.$company;
 echo exec('./traditional.sh '.$company);
-
+//$json =  json_decode(exec('./traditional.sh '.$company), true);
+//echo $json
 ?>
