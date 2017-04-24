49a98d11be25d624e16d3a31fb7fb56d39553d21
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-04-24 20:18:44 +0530

    Update market-news.php

--- view/php/market-news.php
+++ view/php/market-news.php
@@ -1,10 +1,10 @@
-
 <?php
 	 //php 7.0
- 	echo "inside market-news.php"
- 	//header("Location:/Research-Platform-Stock-Market/view/market-news.html")
+ 	echo "inside market-news.php";
+ 	//header("Location:/Research-Platform-Stock-Market/view/market-news.html");
  	$company_id = $_GET['company_id'];
- 	echo exec bash market_news.sh $company_id
- 	echo "success"
+ 	//company_id="REL";
+ 	echo exec bash market_news.sh $company_id;
+ 	echo "success";
  	
  ?>
