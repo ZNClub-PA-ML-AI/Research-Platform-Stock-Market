1651d1aa4b2dfd04fce9967474e3234c34cd8987
melwyn95 <melwyn95@gmail.com>
2017-04-24 13:46:23 +0000

    in process of automating,first need to pull

--- view/php/market_news.sh
+++ view/php/market_news.sh
@@ -1,10 +1,24 @@
 
 #run python scripts for market-news module
 
-#take company_id as input
-company_id=$1
+#cd to etl dir
+#cd ../../module/market-news/src/etl
+#
+#run python modules
+#echo "module 1"
+python keyword_extraction_v3.py $company_id
 
-python ../../module/market-news/src/model/linear_model.py $company_id
+echo "module 2"
+#python preprediction.py $company_id
+ 
+
+
+#take company_id as input
+##company_id=$1
+
+cd ../../module/market-news/src/model
+
+python linear_model.py $company_id
 
 #python test_sh.py $company_id
 
