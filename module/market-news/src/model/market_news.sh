7de1e17d2324336489ece76a9e318255c827a38c
melwyn95 <melwyn95@gmail.com>
2017-04-24 13:15:39 +0000

    shell vmd works in model dir, but now testing in php dir

--- module/market-news/src/model/market_news.sh
+++ module/market-news/src/model/market_news.sh
@@ -4,7 +4,9 @@
 #take company_id as input
 company_id=$1
 
-python ../../module/market-news/src/model/linear_model.py $company_id
+#python ../../module/market-news/src/model/linear_model.py $company_id
+
+python linear_model.py $company_id
 
 #python test_sh.py $company_id
 
