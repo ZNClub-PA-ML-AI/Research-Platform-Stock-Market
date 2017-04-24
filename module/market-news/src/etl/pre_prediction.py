ed8b2ed81d2796bc4c89067633fbe4d0d191e189
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-04-24 19:06:11 +0530

    Update pre_prediction.py

--- module/market-news/src/etl/pre_prediction.py
+++ module/market-news/src/etl/pre_prediction.py
@@ -44,6 +44,54 @@ def is_competitor(title,intro,body,company):
 
     for i,r in df2.iterrows():
         if str(r.company)==company:
+    for i,r in df2.iterrows():
+        if str(r.company)==company:
+"""
+Created on Thu Mar 23 20:58:21
+
+@author: ZNevzz
+"""
+import datetime
+import pandas as pd
+import collections
+import re
+import platform
+import sys
+
+
+if platform.system()=='Windows':
+    company_id,mode='TCS','close'
+else:
+    company_id,mode=sys.argv[1],sys.argv[2]
+
+filenames=['../../'+company_id+'.csv']
+
+df=pd.read_csv(filenames[0])
+date_list = df.date.tolist()
+dates=set(date_list)
+#print(len(dates))
+
+result=pd.DataFrame()
+cnt = collections.Counter(date_list)
+#print(len(cnt))
+
+od = collections.OrderedDict(sorted(cnt.items()))
+
+score=dict()
+for k,v in od.items():
+    print(k,v)
+    score[k]=0
+
+## competitor def
+def is_competitor(title,intro,body,company):
+    
+    df1 = pd.read_excel('../../../data/explicit/company_keyword.xlsx','Sheet1')
+    df2 = pd.read_excel('../../../data/explicit/company_keyword.xlsx','Sheet2')
+    competitors=list()
+    competitor=collections.defaultdict(list)
+
+    for i,r in df2.iterrows():
+        if str(r.company)==company:
             competitors=str(r.competitors).split(',')
             break
     
@@ -73,7 +121,7 @@ for i,r in df.iterrows():
     sc=float(r.score)
     
     # before markets open
-    if time<=time_open and filter=='open':
+    if time<=time_open and mode=='open':
          
          if len(next_date)>0:
             score[date]+=sum(next_date)
@@ -83,11 +131,11 @@ for i,r in df.iterrows():
          else:
              score[date]+=sc    
     #after markets close
-    elif time>time_close and filter=='open':
+    elif time>time_close and mode=='open':
          
          next_date.append(sc)
     #during trading hours
-    elif filter=='close':
+    elif mode=='close':
          
          score[date]+=sc
 for k,v in od.items():
@@ -98,6 +146,7 @@ for k,v in od.items():
 
 df=pd.DataFrame(score,index=['score'])
 df=df.transpose()
-df.to_csv('../../data/'+company_id+'_score_'+filter+'.csv',sep=',',encoding='utf-8')
+df.to_csv('../../'+company_id+'_score_'+mode+'.csv',sep=',',encoding='utf-8')
 #df.to_json('TCS_score_open.json')
+
 
