25e8ce051d6e17cf5cd315121ff3e4138792156b
ZNevzz <dsouza.nevil45@gmail.com>
2017-04-24 07:08:44 +0530

    traditional-2.html and hybrid model work started parallelly

--- view/js/market-news.js
+++ view/js/market-news.js
@@ -1,16 +1,16 @@
+company_id='AX';
+
+labeledSpline(company_id);
+dualChartsOpenScore(company_id);
+dualChartsCloseScore(company_id);
 
 
-labeledSpline();
-dualCharts();
-
-
-function labeledSpline(){
+function labeledSpline(company_id){
 	// spline.js
-
 //https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Business-News/95e16a9b/REL_score_open.json
-
 //https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Business-News/4d982b42/data/json/REL_sentiment.json
-$.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Business-News/4d982b42/data/json/REL_sentiment.json', function(data) {
+
+$.getJSON('/Research-Platform-Stock-Market/view/data/json/market-news/'+company_id+'_sentiment.json', function(data) {
 
   //var close_map = data.close_score;
   var size = Object.keys(data.date).length;
@@ -44,7 +44,7 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
       zoomType: 'x'
     },
     title: {
-      text: 'Market News Sentimental Graph'
+      text: 'Market News Sentimental Graph for NSE/'+company_id
     },
     subtitle: {
       text: 'Sentiment-analysis-using-Business-News'
@@ -63,12 +63,12 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
       title: {
         text: 'Sentimental Score'
       },
-      min: -0.30,
+      min: -1.2,
       gridLineWidth: 0,
       alternateGridColor: null,
       plotBands: [{ 
-          from: 0.05,
-          to: -0.05,
+          from: 0.1,
+          to: -0.1,
           color: 'rgba(0, 0, 0, 0)',
           label: {
             text: 'NEUTRAL',
@@ -77,8 +77,8 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
             }
           }
         }, { 
-          from: 0.05,
-          to: 0.2,
+          from: 0.1,
+          to: 0.5,
           color: 'rgba(100, 255, 100, 0.2)',
           label: {
             text: 'POSITIVE',
@@ -87,8 +87,8 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
             }
           }
         }, { // Light air
-          from: 0.2,
-          to: 0.4,
+          from: 0.5,
+          to: 1.0,
           color: 'rgba(100, 255, 100, 0.4)',
           label: {
             text: 'HIGHLY POSITIVE',
@@ -97,8 +97,8 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
             }
           }
         }, { 
-          from: -0.05,
-          to: -0.2,
+          from: -0.1,
+          to: -0.5,
           color: 'rgba(255, 100, 100, 0.2)',
           label: {
             text: 'NEGATIVE',
@@ -107,8 +107,8 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
             }
           }
         },{
-          from: -0.2,
-          to: -0.4,
+          from: -0.5,
+          to: -1.0,
           color: 'rgba(255, 100, 100, 0.5)',
           label: {
             text: 'HIGHLY NEGATIVE',
@@ -132,11 +132,17 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
       }
     },
     series: [{
-      name: 'RELIANCE OP sentiment',
-      data: open
+      name: 'OpenPrice sentiment',
+      data: open,
+	  
+      color: '#00FAA2'
+      
     }, {
-      name: 'RELIANCE CP sentiment',
-      data: close
+      name: 'ClosePrice sentiment',
+      data: close,
+	 
+		color: '#FFE16B'
+     
     }]
   });
 });
@@ -144,18 +150,20 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
 }
 
 
-function dualCharts(){
+function dualChartsOpenScore(company_id){
 // dual.js
 
 //getJSON
 //https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Business-News/54103f1e/data/json/REL_qs.json
 
-$.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Business-News/54103f1e/data/json/REL_qs.json',
-  function(data) {
+$.getJSON('/Research-Platform-Stock-Market/view/data/json/market-news/'+company_id+'_qs.json',function(data) {
     //console.log(data['Unnamed: 0'][0]);
 
-    var price = [];
-    var sentiment = [];
+    var price_open = [];
+    var sentiment_open = [];
+	//var price_close = [];
+    //var sentiment_close = [];
+	
     var size = Object.keys(data.Open).length;
     console.log(size);
 
@@ -173,9 +181,12 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
       }
       var dateUTC = Date.UTC(dates[0], m, dates[2]);
 
-      price.push([dateUTC, data.Open[i]]);
-      sentiment.push([dateUTC, data.open_score[i]]);
-      //console.log(i);
+      price_open.push([dateUTC, data.Open[i]]);
+      sentiment_open.push([dateUTC, data.open_score[i]]);
+	  //price_close.push([dateUTC, data.Close[i]]);
+      //sentiment_close.push([dateUTC, data.close_score[i]]);
+      
+	  //console.log(i);
       //console.log(price[i]);
     }
 
@@ -205,7 +216,7 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
           }
         },
         title: {
-          text: 'Stock Open Price',
+          text: 'Stock Price',
           style: {
             color: Highcharts.getOptions().colors[1]
           }
@@ -239,10 +250,11 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
         backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
       },
       series: [{
-        name: 'Sentiment',
+        name: 'Open Price Sentiment',
         type: 'spline',
         yAxis: 1,
-        data: sentiment         
+        data: sentiment_open  ,
+		color: '#00FAA2'		
           ,
         tooltip: {
           valueSuffix: ' '
@@ -251,14 +263,352 @@ $.getJSON('https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Busin
       }, {
         name: 'Open Price',
         type: 'spline',
-        data: price        
+        data: price_open  ,
+		color: '#00B7FF'
           ,
         tooltip: {
           valueSuffix: 'Rs'
         }
-      }]
+      }
+	  ]
     });
 
   });
 
 }
+
+function dualChartsCloseScore(company_id){
+// dual.js
+
+//getJSON
+//https://cdn.rawgit.com/ZNClub-PA-ML-AI/Sentiment-analysis-using-Business-News/54103f1e/data/json/REL_qs.json
+
+$.getJSON('/Research-Platform-Stock-Market/view/data/json/market-news/'+company_id+'_qs.json',function(data) {
+    //console.log(data['Unnamed: 0'][0]);
+
+    //var price_open = [];
+    //var sentiment_open = [];
+	var price_close = [];
+    var sentiment_close = [];
+	
+    var size = Object.keys(data.Open).length;
+    console.log(size);
+
+    for (var i = 0; i < size; i += 1) {
+      var temp = data['Unnamed: 0'][i];
+      //split date ie key
+
+      var dates = temp.split('-');
+      //adjust month
+      var m = dates[1];
+      m = parseInt(m) - 1;
+      m = m.toString()
+      if (m.length == 1) {
+        m = "0" + m;
+      }
+      var dateUTC = Date.UTC(dates[0], m, dates[2]);
+
+      //price_open.push([dateUTC, data.Open[i]]);
+      //sentiment_open.push([dateUTC, data.open_score[i]]);
+	  price_close.push([dateUTC, data.Close[i]]);
+      sentiment_close.push([dateUTC, data.close_score[i]]);
+      
+	  //console.log(i);
+      //console.log(price[i]);
+    }
+
+
+
+
+    Highcharts.chart('container4', {
+      chart: {
+        zoomType: 'x'
+      },
+      title: {
+        text: 'Close Price and Market sentiment'
+      },
+      subtitle: {
+        text: 'Sentiment-analysis-using-Business-News'
+      },
+      xAxis: [{
+        type: 'datetime',
+        //categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        crosshair: true
+      }],
+      yAxis: [{ // Primary yAxis
+        labels: {
+          format: '{value} Rs',
+          style: {
+            color: Highcharts.getOptions().colors[1]
+          }
+        },
+        title: {
+          text: 'Stock Price',
+          style: {
+            color: Highcharts.getOptions().colors[1]
+          }
+        }
+      }, { // Secondary yAxis
+        title: {
+          text: 'Sentiment Score',
+          style: {
+            color: Highcharts.getOptions().colors[0]
+          }
+        },
+        labels: {
+          format: '{value}',
+          style: {
+            color: Highcharts.getOptions().colors[0]
+          }
+        },
+        opposite: true
+      }],
+      tooltip: {
+        shared: true
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'left',
+        x: 70,
+        y: 12,
+        verticalAlign: 'top',
+
+        floating: true,
+        backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+      },
+      series: [{
+        name: 'Close Price Sentiment',
+        type: 'spline',
+        yAxis: 1,
+        data: sentiment_close         
+          ,color: '#FFE16B',
+        tooltip: {
+          valueSuffix: ' '
+        }
+
+      },{
+        name: 'Close Price',
+        type: 'spline',        
+        data: price_close         
+          ,color: '#00B7FF',
+        tooltip: {
+          valueSuffix: 'Rs'
+        }
+
+      }
+	  ]
+    });
+
+  });
+
+}
+
+/*
+company_id='AX';
+
+candlestick(company_id);
+//predictionChart();
+
+function candlestick(company_id){
+	
+//[1269820800000,33.29,33.41,33.09,33.20,135185785],
+
+$.getJSON('/Research-Platform-Stock-Market/view/js/data/NSE-'+company_id+'.json', function(data) {
+  var open = data['Open'];
+  var high = data['High'];
+  var low = data['Low'];
+  var close = data['Close'];
+  var size = Object.keys(close).length;
+  var volume = data["Total Trade Quantity"];
+  var turnover = data["Turnover (Lacs)"];
+  var date = data["Date"];
+
+  var ohlc = [];
+  var vol = [];
+  var turn = [];
+
+  // set the allowed units for data grouping
+  var groupingUnits = [
+      [
+        'week', // unit name
+        [1] // allowed multiples
+      ],
+      [
+        'month', [1, 2, 3, 4, 6]
+      ]
+    ],
+
+    i = size-1;
+
+
+  for (i; i >=0; i -= 1) {
+    //format date to utc
+    var dates = date[i].split('-');
+    //adjust month
+    var m = dates[1];
+    m = parseInt(m) - 1;
+    m = m.toString();
+    if (m.length == 1) {
+      m = "0" + m;
+    }
+    var dateUTC = Date.UTC(dates[0], m, dates[2]);
+		
+    ohlc.push([
+      dateUTC,
+      open[i],
+      high[i],
+      low[i],
+      close[i]
+    ]);
+
+    vol.push([
+      dateUTC,
+      volume[i]
+    ]);
+    
+    turn.push([
+    	dateUTC,
+      turnover[i]
+    ]);
+    //console.log(vol);
+  }
+
+
+  // create the chart
+  Highcharts.stockChart('container1', {
+
+    rangeSelector: {
+      selected: 1
+    },
+
+    title: {
+      text: 'NSE/REL Stock Price'
+    },
+
+    yAxis: [{
+      labels: {
+        align: 'right',
+        x: -3
+      },
+      title: {
+        text: 'OHLC'
+      },
+      height: '60%',
+      lineWidth: 2
+    }, {
+      labels: {
+        align: 'right',
+        x: -3
+      },
+      title: {
+        text: 'Volume'
+      },
+      top: '75%',
+      height: '25%',
+      offset: 0,
+      lineWidth: 2
+    }
+],
+
+    tooltip: {
+      split: true
+    },
+
+    series: [{
+      type: 'candlestick',
+      name: 'RELIANCE',
+      data: ohlc,
+      dataGrouping: {
+        units: groupingUnits
+      }
+    }, {
+      type: 'spline',
+      name: 'Volume',
+      data: vol,
+      yAxis: 1,
+      dataGrouping: {
+        units: groupingUnits
+      }
+    }]
+  });
+});
+
+}
+*/
+/*
+function predictionChart(){
+
+$.getJSON('/Research-Platform-Stock-Market/view/js/data/traditional.json', function(data) {
+
+  var size = Object.keys(data.date).length;
+  var close = [];
+  var open = [];
+
+  for (var i = 0; i < size; i += 1) {
+    console.log(data.date[i]);
+    var dates = data.date[i].split('-');
+    //adjust month
+    var m = dates[1];
+    m = parseInt(m) - 1;
+    m = m.toString()
+    if (m.length == 1) {
+      m = "0" + m;
+    }
+    var dateUTC = Date.UTC(dates[0], m, dates[2]);
+
+    open.push([dateUTC, data.Open[i]]);
+    close.push([dateUTC, data.open_predicted[i]]);
+
+  }
+
+  Highcharts.chart('container1', {
+    chart: {
+      type: 'spline',
+      zoomType: 'xy'
+    },
+    title: {
+      text: 'Open Price - Predicted Open Price TRADITIONAL'
+    },
+    subtitle: {
+      text: ''
+    },
+    xAxis: {
+      type: 'datetime',
+      dateTimeLabelFormats: { // don't display the dummy year
+        month: '%e. %b',
+        year: '%b'
+      },
+      title: {
+        text: 'Months'
+      }
+    },
+    yAxis: {
+      title: {
+        text: 'Open Price'
+      },
+      min: -0.30,
+      gridLineWidth: 0,
+      alternateGridColor: null,      
+    },
+    tooltip: {
+      headerFormat: '<b>{series.name}</b><br>',
+      pointFormat: '{point.x:%e. %b}: {point.y:.3f} value'
+    },
+    plotOptions: {
+      spline: {
+        marker: {
+          enabled: true
+        }
+      }
+    },
+    series: [{
+      name: 'Open Price',
+      data: open
+    }, {
+      name: 'Predicted Open Price',
+      data: close
+    }]
+  });
+});
+}
+*/
