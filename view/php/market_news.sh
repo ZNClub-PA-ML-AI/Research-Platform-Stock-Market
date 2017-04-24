03712a11ce771369aa441d336af6d0158c0c3fef
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-04-24 19:45:11 +0530

    Update market_news.sh

--- view/php/market_news.sh
+++ view/php/market_news.sh
@@ -1,32 +1,42 @@
 
-#run python scripts for market-news module
 
-cd to etl dir
-cd ../../module/market-news/src/etl
+#run python scripts for market-news module 
 
-#run python modules
-echo "module 1"
-python keyword_extraction_v3.py $company_id
+#take company_id as input 
+company_id=$1 
 
-echo "module 2"
-#python preprediction.py $company_id
- 
+#cd to etl dir 
+cd ../../module/market-news/src/etl 
 
+#run python modules 
 
-#take company_id as input
-##company_id=$1
+echo "module 1" 
+python keyword_extraction_v3.py $company_id 
 
+echo "module 2" 
+python pre_prediction.py $company_id open
 
-echo "exec model"
-cd ../model
-
-python linear_model.py $company_id
-
-#python test_sh.py $company_id
-
-#end of file
+python pre_prediction.py $company_id close
 
 
+echo "module 3"
+python merge_sentiment.py $company_id
+
+echo "module 4"
+python merge_quandl.py $company_id
+
+
+echo "module 5"
+#python csv_json.py $company_id NSE
+python csv_json.py $company_id qs
+python csv_json.py $company_id sentiment
+
+echo "exec model" 
+cd ../model 
+
+python linear_model.py $company_id 
+
+ #end of file 
 
 
 
