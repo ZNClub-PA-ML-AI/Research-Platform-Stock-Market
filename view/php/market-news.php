eb7a48c7fd9a13c47e8860e59535901c80e5b163
ZNevzz <dsouza.nevil45@gmail.com>
2017-04-25 11:51:52 +0000

    test with exec

--- view/php/market-news.php
+++ view/php/market-news.php
@@ -1,10 +1,11 @@
 <?php
 	 //php 7.0
  	echo "inside market-news.php";
- 	header("Location:/Research-Platform-Stock-Market/view/market-news.html");
- 	//$company_id = $_GET['company_id'];
+ 	//header("Location:/Research-Platform-Stock-Market/view/market-news.html");
+ 	$company_id = $_GET['cid'];
  	//company_id="REL";
- 	//echo exec bash market_news.sh $company_id;
+ 	//echo exec("sudo ./market_news.sh "$company_id);
+	exec("sudo ./test.sh");
  	echo "success";
  	
  ?>
---
+++ view/php/test.sh
@@ -0,0 +1,3 @@
+echo ZNevzz
+$inp=$1
+
