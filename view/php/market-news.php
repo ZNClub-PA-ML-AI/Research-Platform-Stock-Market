665ffc68bc7a68b62c177f437395b8b3ec8697a0
ZNevzz <dsouza.nevil45@gmail.com>
2017-04-25 11:53:27 +0000

    looks like bug has been fixed

--- view/php/market-news.php
+++ view/php/market-news.php
@@ -1,11 +1,11 @@
 <?php
 	 //php 7.0
  	echo "inside market-news.php";
- 	//header("Location:/Research-Platform-Stock-Market/view/market-news.html");
+ 	header("Location:/Research-Platform-Stock-Market/view/market-news.html");
  	$company_id = $_GET['cid'];
  	//company_id="REL";
- 	//echo exec("sudo ./market_news.sh "$company_id);
-	exec("sudo ./test.sh");
+ 	echo exec("sudo ./market_news.sh .$company_id);
+	//exec("sudo ./test.sh ".$company_id);
  	echo "success";
  	
  ?>
