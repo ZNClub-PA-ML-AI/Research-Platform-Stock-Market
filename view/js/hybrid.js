7a4cec7a62bde9152d6f26981ff302380128ff6d
ZNevzz <dsouza.nevil45@gmail.com>
2017-04-26 14:10:15 +0000

    test hybrid js

--- view/js/hybrid.js
+++ view/js/hybrid.js
@@ -4,6 +4,7 @@ var company_id='REL';
 
 
 
+/*
 $.getJSON('/Research-Platform-Stock-Market/view/php/market-news.php?cid='+company_id, function(data) {
 	console.log(data);
 	console.log(data.result);
@@ -16,13 +17,15 @@ $.getJSON('/Research-Platform-Stock-Market/view/php/market-news.php?cid='+compan
 	}
 	
 });
+*/
 
+function candlestick(company_id){
 
-function labeledSpline(company_id){
+$.getJSON('/Research-Platform-Stock-Market/view/data/json/market-news/NSE-'+company_id+'.json', function(data) {
 
-$.getJSON('/Research-Platform-Stock-Market/view/data/json/hybrid/'+company_id+'_sentiment.json', function(data) {
+#var size = Object.keys(data.date).length;
+console.log(data);
 
-  var size = Object.keys(data.date).length;
   
 });
 
