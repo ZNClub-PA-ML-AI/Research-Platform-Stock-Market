9d5d363fb39320970aef52f0bf8ae6e3346b74cf
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-04-26 23:25:22 +0530

    removed hardcoded part

--- view/php/market-news.php
+++ view/php/market-news.php
@@ -3,8 +3,8 @@
  	//echo "inside market-news.php";
  	//header("Location:/Research-Platform-Stock-Market/view/market-news.html");
 
- 	//$company_id = $_GET['cid']; 	
-	$company_id = 'REL';
+ 	$company_id = $_GET['cid']; 	
+	//$company_id = 'REL';
  	exec("sudo ./market_news.sh ".$company_id);
 	
 	// delay return of control
