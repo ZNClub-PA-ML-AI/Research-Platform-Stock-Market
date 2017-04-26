f9a045d660c8023189dd6bc602dfcb3076bfd327
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-04-26 23:58:43 +0530

    REMOVED absolute path

--- view/php/market_news.sh
+++ view/php/market_news.sh
@@ -11,35 +11,37 @@
 company_id=$1 
 
 #cd to etl dir 
-#cd ../../module/market-news/src/etl 
+cd ../../module/market-news/src/etl 
 
 #run python modules 
 
 echo "module 1" 
-python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/keyword_extraction_v3.py $company_id 
+#python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/keyword_extraction_v3.py $company_id 
+python keyword_extraction_v3.py $company_id 
 
 echo "module 2" 
-python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/pre_prediction.py $company_id open
+#python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/pre_prediction.py $company_id open
+python pre_prediction.py $company_id open
 
-python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/pre_prediction.py $company_id close
+python pre_prediction.py $company_id close
 
 
 echo "module 3"
-python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/merge_sentiment.py $company_id
+python merge_sentiment.py $company_id
 
 echo "module 4"
-python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/merge_quandl.py $company_id
+python merge_quandl.py $company_id
 
 
 echo "module 5"
-python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/csv_json.py $company_id NSE
-python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/csv_json.py $company_id qs
-python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/etl/csv_json.py $company_id sentiment
+python csv_json.py $company_id NSE
+python csv_json.py $company_id qs
+python csv_json.py $company_id sentiment
 
 echo "exec model" 
-#cd ../model 
+cd ../model 
 
-python /var/www/html/Research-Platform-Stock-Market/module/market-news/src/model/linear_model.py $company_id 
+python model/linear_model.py $company_id 
 
  #end of file 
 
