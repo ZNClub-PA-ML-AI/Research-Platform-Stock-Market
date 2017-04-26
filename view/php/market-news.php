dc1269a769ef4b0be7676e52a4a72c651b18b325
Nevil Dsouza <dsouza.nevil45@gmail.com>
2017-04-26 22:54:26 +0530

    while loop to delay php return of control

--- view/php/market-news.php
+++ view/php/market-news.php
@@ -1,12 +1,16 @@
 <?php
-	 //php 7.0
+	//php 7.0
  	//echo "inside market-news.php";
  	//header("Location:/Research-Platform-Stock-Market/view/market-news.html");
- 	$company_id = $_GET['cid'];
- 	//company_id="REL";
+
+ 	$company_id = $_GET['cid']; 	
  	exec("sudo ./market_news.sh ".$company_id);
-	//exec("sudo ./test.sh ".$company_id);
- 	//echo "success";
+	
+	// delay return of control
+	$x = 1; 
+	while($x <= 1050) {	    
+	    $x++;
+	} 
 
 	$data=['result'=>'success'];
 	header('Content-Type:application/json');
