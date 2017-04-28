4fd1a92a8f58961c0049ceb8416e5aef88e2ede5
Crystal Cuthinho <cuthinho.crystal95@gmail.com>
2017-04-28 08:49:11 +0530

    Update twitter.sh

--- view/php/twitter.sh
+++ view/php/twitter.sh
@@ -1,3 +1,3 @@
-python /var/www/html/Research-Platform-Stock-Market/module/twitter/src/code3 - average_sentiment.py "${1}.csv"
-python /var/www/html/Research-Platform-Stock-Market/module/twitter/src/code4 - merge_with_NSE & find correlation.py "${1}.csv" "${2}.csv"
-python /var/www/html/Research-Platform-Stock-Market/module/twitter/src/code5 - prediction.py "${1}.csv"
+python /var/www/html/Research-Platform-Stock-Market/module/twitter/src/code3 - average_sentiment.py "labeled.csv"
+python /var/www/html/Research-Platform-Stock-Market/module/twitter/src/code4 - merge_with_NSE & find correlation.py "Reliance_score.csv" "NSE-RELIANCE.csv"
+python /var/www/html/Research-Platform-Stock-Market/module/twitter/src/code5 - prediction.py "merged_with_NSE_data.csv"
